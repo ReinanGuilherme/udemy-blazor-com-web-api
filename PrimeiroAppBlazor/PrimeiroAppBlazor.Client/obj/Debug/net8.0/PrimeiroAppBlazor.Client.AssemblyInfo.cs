0b743596865e4f612c925831d80e21e8180ac6f3
ReinanGuilhermeOffice <reinan.neto@grupofasitec.com.br>
2024-06-07 16:32:06 -0300

    secao-3.16

--- PrimeiroAppBlazor/PrimeiroAppBlazor.Client/obj/Debug/net8.0/PrimeiroAppBlazor.Client.AssemblyInfo.cs
+++ PrimeiroAppBlazor/PrimeiroAppBlazor.Client/obj/Debug/net8.0/PrimeiroAppBlazor.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroAppBlazor.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+defbcb0b2c0966579bb0ca92c1e25a3045af82a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7a9a0d163b0609d425659fe147beb33800d398")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroAppBlazor.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroAppBlazor.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
