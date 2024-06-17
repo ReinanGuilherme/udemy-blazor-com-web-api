0610c85084e09682a8d0cd8745b8d18ea7cb32e3
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.com.br>
2024-06-17 13:34:15 -0300

    secao-7.40

--- PrimeiroAppBlazor/PrimeiroAppBlazor.Client/Services/CursoService.cs
+++ PrimeiroAppBlazor/PrimeiroAppBlazor.Client/Services/CursoService.cs
@@ -7,6 +7,7 @@ namespace PrimeiroAppBlazor.Client.Services
 		public List<CursoCLS> lista;
 		public event Func<Task> OnChange;
 		public event Func<CursoCLS, Task> OnEdit;
+		public event Func<CursoCLS, Task> OnSearch;
 		public CursoService()
         {
 			lista = new List<CursoCLS>();
@@ -28,6 +29,21 @@ namespace PrimeiroAppBlazor.Client.Services
 		{
 			return lista;
 		}
+		public List<CursoCLS> BuscarCursos(CursoCLS cursoCLS)
+		{
+			List<CursoCLS> listaTotal = ListarCursos();
+			if (string.IsNullOrWhiteSpace(cursoCLS.nombrecurso))
+			{
+				listaTotal = listaTotal.Where(p => p.nombrecurso.ToUpper().Contains(cursoCLS.nombrecurso.ToUpper())).ToList();
+			}
+
+			if(cursoCLS.idcarrera != 0)
+			{
+				listaTotal = lista.Where(p => p.idcarrera == cursoCLS.idcarrera).ToList();
+            }
+
+			return listaTotal;
+		}
 
 		public void Agregar(CursoCLS curso)
 		{
@@ -39,17 +55,21 @@ namespace PrimeiroAppBlazor.Client.Services
 		{
 			OnChange.Invoke();
 		}
-		
-		public void NotificarEditar(CursoCLS cursoCLS)
+        public void Eliminar(int idcurso)
+        {
+            CursoCLS cursoCLS = lista.Where(p => p.idcurso == idcurso).First();
+            lista.Remove(cursoCLS);
+            NotificarCambios();
+        }
+        public void NotificarEditar(CursoCLS cursoCLS)
 		{
 			OnEdit.Invoke(cursoCLS);
 		}
-
-		public void Eliminar(int idcurso)
+		public void NotificarSearch(CursoCLS cursoCLS)
 		{
-			CursoCLS cursoCLS = lista.Where(p => p.idcurso == idcurso).First();
-			lista.Remove(cursoCLS);
-			NotificarCambios();
+			OnSearch.Invoke(cursoCLS);
 		}
+
+		
     }
 }
